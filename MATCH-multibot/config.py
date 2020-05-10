2ec275d48ebfc87c5a55a76be429c56d42c25886
Yarb <jstrandman@gmail.com>
2020-05-10 15:22:20 +0300

    Add files via upload
    config.py for easier settings management

---
+++ MATCH-multibot/config.py
@@ -0,0 +1,24 @@
+# Discord token and guild(Channel) name
+
+DISCORD_TOKEN=""
+DISCORD_GUILD=""
+
+
+# Number of time the same offset will be used
+OFFSET_COUNT = 5
+
+# Set this to prevent multiple registrations with same name
+NO_DUPLICATES = 0
+
+# Initial time and warning intervals
+START = 150
+WARN1 = 60
+WARN2 = 20
+
+
+
+# M.A.T.C.H. Status codes
+IDLE = 0
+REGISTRATION = 1
+RUNNING = 2
+ERROR = -1
